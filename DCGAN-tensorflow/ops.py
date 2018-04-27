9e897fedf1e18a3a1449143c0542dc827976863d
Wentian Bao <papermoon_shine@126.com>
2018-04-27 18:12:49 -0400

    edit ops.py

--- DCGAN-tensorflow/ops.py
+++ DCGAN-tensorflow/ops.py
@@ -27,21 +27,13 @@ else:
   def concat(tensors, axis, *args, **kwargs):
     return tf.concat(tensors, axis, *args, **kwargs)
 
-class batch_norm(object):
-  def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
-    with tf.variable_scope(name):
-      self.epsilon  = epsilon
-      self.momentum = momentum
-      self.name = name
-
-  def __call__(self, x, train=True):
-    return tf.contrib.layers.batch_norm(x,
-                      decay=self.momentum, 
-                      updates_collections=None,
-                      epsilon=self.epsilon,
-                      scale=True,
-                      is_training=train,
-                      scope=self.name)
+def batch_norm(x, train=True, epsilon=1e-5, momentum = 0.9):
+  return tf.contrib.layers.batch_norm(x,
+                    decay=self.momentum, 
+                    updates_collections=None,
+                    epsilon=self.epsilon,
+                    scale=True,
+                    is_training=train)
 
 def conv_cond_concat(x, y):
   """Concatenate conditioning vector on feature map axis."""
@@ -68,7 +60,7 @@ def conv2d(input_, output_dim,
 
 def deconv2d(input_, output_shape,
        k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-       name="deconv2d", with_w=False):
+       name="deconv2d"):
   """
   deconvolution operation for GAN
   """
@@ -77,22 +69,11 @@ def deconv2d(input_, output_shape,
     w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
               initializer=tf.random_normal_initializer(stddev=stddev))
     
-    try:
-      deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
-                strides=[1, d_h, d_w, 1])
+    deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+              strides=[1, d_h, d_w, 1])
 
-    # Support for verisons of TensorFlow before 0.7.0
-    except AttributeError:
-      deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
-                strides=[1, d_h, d_w, 1])
-
-    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
     deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
-
-    if with_w:
-      return deconv, w, biases
-    else:
-      return deconv
+    return deconv
      
 def lrelu(x, leak=0.2, name="lrelu"):
   """
